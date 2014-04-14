2c4a4ae8e09d566e017328cbb83f8bc13620ac53
qiubix <qiubix@gmail.com>
2014-04-14 11:57:29 +0200

    Poprawiona funkcja, dopisane stale.

--- reaktor.m
+++ reaktor.m
@@ -1,3 +1,15 @@
 function dy = reaktor(t, y)
-dy(1) = F_in*C_Ain - F*C_A - V*k_0*exp(-E/R*T)*C_A
-dy(2) = F_in*ro*c_p*T_in - F*ro*c_p*T + V*h*k_0*exp(-E/R*T)*C_A - a*F_c^(b+1)*(T-T_Cin) / (F_C + a*F_c^b / 2*ro_c*c_pc)
+
+% stale
+ro = ro_c = 10^6;
+c_p = c_pc = 1;
+k_0 = 10^10;
+E_R = 8330.1;
+h = 130*10^6;
+a = 0.516*10^6;
+b = 0.5;
+
+% funkcja
+dy(1) = F_in*C_Ain - F*C_A - V*k_0*exp(-E_R/T)*C_A;
+dy(2) = F_in*ro*c_p*T_in - F*ro*c_p*T + V*h*k_0*exp(-E_R/T)*C_A - a*F_c^(b+1)*(T-T_Cin) / (F_C + a*F_c^b / 2*ro_c*c_pc);
+end
